4b568a036a122aac80b00c7629cc76294dc38566
JohannFaust9937 <111689269+JohannFaust9937@users.noreply.github.com>
2023-06-21 17:10:40 +0300

    update and separate to cpp/h files

---
+++ main.cpp
@@ -0,0 +1,29 @@
+﻿#include "sorting.h"
+
+int main()
+{
+	const int size{ 10 };
+
+	// Пузырьковая сортировка
+	int array0[size]{ 0, 9, 1, 8, 5, 5, 3, 6, 4, 5 };
+	printf("Bubble sorting example result\n");
+	bubbleSort(size, array0);
+	printArray(size, array0);
+	printf("\n");
+
+	// Сортировка выбором
+	int array1[size]{ 0, 9, 1, 8, 5, 5, 3, 6, 4, 5 };
+	printf("Selection sorting example result\n");
+	selectionSort(size, array1);
+	printArray(size, array1);
+	printf("\n");
+
+	// Сортировка вставками
+	int array2[size]{ 0, 9, 1, 8, 5, 5, 3, 6, 4, 5 };
+	printf("Insertion sorting example result\n");
+	insertionSort(size, array2);
+	printArray(size, array2);
+	printf("\n");
+
+	return 0;
+}
---
+++ sorting.h
@@ -0,0 +1,7 @@
+#pragma once
+#include <cstdio>
+
+void printArray(const int, int* const);
+void bubbleSort(const int, int*);
+void selectionSort(const int, int*);
+void insertionSort(const int, int*);
